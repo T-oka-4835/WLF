7eb60a9ddaeaaf1c528f275243497a4b8acd02a8
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-10-23 14:04:01 +0000

    [Add]Top画面と、Userコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'homes#top'
+  get 'homes/about'
+  resources :post_images, only: [:new, :create, :index, :show]
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
