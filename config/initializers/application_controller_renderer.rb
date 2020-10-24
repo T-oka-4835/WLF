78f98bf0c2d99591f5027cac88be5794c8c8ab82
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-10-24 05:54:37 +0000

    [Fix]投稿画像が一覧画面に出るよう修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '97c2b781e25b874b7a28a62f27dd9af2bd3022b324236afe77600fd1ee41dd9ccb7298b348ba4fe47a70bec76370b8323b55b96d3fa67663ccc8d3b499a783df'
